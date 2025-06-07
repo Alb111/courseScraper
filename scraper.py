c005c790d90cc162b8692ab7959be0e134b93119
alb123 <albertfelix2005@gmail.com>
2025-06-06 19:21:19 -0700

    got bsoe course links

--- scraper.py
+++ scraper.py
@@ -1,8 +1,16 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag, ResultSet
 
-URL: str = "https://pisa.ucsc.edu/class_search/"
+# Get HTML from BSOE course website
+URL: str = "https://courses.engineering.ucsc.edu/courses"
 page: requests.Response = requests.get(URL)
 
-# soup: BeautifulSoup = BeautifulSoup(page.content, "html.parser")
-print(page.text)
+# Put into a soup object and parse for li
+soup: BeautifulSoup = BeautifulSoup(page.content, "html.parser")
+course_links: ResultSet = soup.find_all("li")
+
+print(type(course_links[0]))
+print(type(course_links))
+
+
+# print(page.text)
